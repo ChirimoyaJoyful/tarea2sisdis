72c78f3d10aea46f7032b093c17f64dd2467a166
ChirimoyaJoyful <81930823+ChirimoyaJoyful@users.noreply.github.com>
2022-05-23 23:24:49 -0400

    Add files via upload

--- app/index.js
+++ app/index.js
@@ -4,25 +4,9 @@ const { Kafka } = require('kafkajs');
 const { Partitioners } = require('kafkajs')
 
 
-
-// CONSUMIDOR
-// var consumer = new kafka.Consumer(client, [{topic: 'login'}]);
-
-// consumer.on('message', function (message) {
-//     console.log(message);
-
-// });
-
-//PRODUCTOR
-
-
-
-
-
 const port = 3000;
-
 const app = express();
-const app2 = express();
+
 
 const kafka = new Kafka({
     clientId: 'my-app',
@@ -30,11 +14,7 @@ const kafka = new Kafka({
 })
 
 const producer = kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner });  
-const consumer = kafka.consumer({ groupId: 'tarea'});
 
-
-
-const users = [];
 app.post('/login', async(req, res) =>{
     var term = { user: req.query.usr , password: req.query.pw };
     console.log(term.user);
@@ -46,42 +26,12 @@ app.post('/login', async(req, res) =>{
         ],
     });
     await producer.disconnect();
-
-
+    res.sendStatus(200);
 });
-
-
-
-
-app2.get('/blocked', async(req, res) =>{
-    await consumer.connect();
-    await consumer.subscribe({topic: 'login',fromBegining: true });
-    await consumer.run({
-        eachMessage: async ({ topic, partition, message }) => {
-            users_blocked.push({"user": message.value.toString(), "time":message.timestamp.toString()});
-            /*console.log({
-                value: message.value.toString(),
-                timestamp: message.timestamp.toString(),
-            });*/
-            console.log(users_blocked);
-        },
-    })
-    for(let i = users_blocked.length - 1; i > 0; i--){
-        if(users_blocked[i].user ){
-
-        };
-    };
-    res.send(JSON.stringify(users_blocked));
-});
-
 
 app.use(express.json());
 
 app.listen(port, () =>{
     console.log('Server running on port', port);
-});
-
-app2.listen(3001, () =>{
-    console.log('Server running on port', 3001);
 });
 
---
+++ security/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const { Kafka } = require('kafkajs');
+const { Partitioners } = require('kafkajs')
+
+
+const port = 3001;
+
+const app = express();
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['kafka:9092'],
+})
+ 
+const consumer = kafka.consumer({ groupId: 'tarea'});
+
+
+
+app.get('/blocked', async(req, res) =>{
+    var users = [];
+    var bloqueados = [];
+    await consumer.connect();
+    await consumer.subscribe({topic: 'login',fromBeginning: true });
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            users.push({"user": message.value.toString(), "time":message.timestamp.toString()});
+            var usuario = users[users.length - 1];
+            var a = 0;
+            var b = 0;
+            for(let i = users.length - 1; i >= 0; i--){
+                if(users[i].user == usuario.user ){
+                    a++;
+                    if (a == 5){
+                        var tf = users[i].time - usuario.time;
+                        if(tf < 60000){
+                            bloqueados.push({"usr": usuario.user});    
+                        }; 
+                    };
+                };
+            }; 
+        console.log(bloqueados);    
+        },
+    })
+    
+});
+
+
+app.use(express.json());
+
+app.listen(port, () =>{
+    console.log('Server running on port', port);
+});
